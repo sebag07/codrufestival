ccd68beb333a022cbd8328efdad8be1274e9c316
sebastiang07 <sebastian@verticalhq.ca>
2023-07-10 10:41:55 +0300

    Added sections

--- wp-content/themes/Divi-child/codrufestival-2023live.php
+++ wp-content/themes/Divi-child/codrufestival-2023live.php
@@ -18,7 +18,7 @@
                     href="https://bilete.codrufestival.ro/"><?php echo get_field('hero_button_text')?></a>
                 <h2 class="heroFocusedText heroDescription"><?php echo get_field('hero_section_title')?></h2>
                 <p class="anim">
-                <?php echo get_field('hero_section_text')?>
+                    <?php echo get_field('hero_section_text')?>
                 </p>
                 <a class="heroContentButton mobileButton mobileContentButton anim"
                     href="https://bilete.codrufestival.ro/"><?php echo get_field('hero_button_text')?></a>
@@ -150,60 +150,98 @@
     </div>
 </div>
 
-<div id="brandCultureAnchor" class="container-fluid sectionPadding">
-    <div class="container">
-      <h2 class="sectionTitle">BRAND'S CULTURE</h2>
-        <div class="row">
-            <?php 
+<section id="brandCultureAnchor">
+    <div class="container-fluid sectionPadding">
+        <div class="container">
+            <h2 class="sectionTitle">BRAND'S CULTURE</h2>
+            <div class="row">
+                <?php 
             $options = get_field("brand_culture", "options"); 
             foreach($options as $option): 
             $white = $option['black_text'] == '1' ? 'text-black' : 'text-white';
             ?>
-            <a class="col-xl-4 col-lg-6 col-md-6 col-sm-12 brandCultureContainer" data-fslightbox="custom-text" data-class="d-block" href="#<?php echo $option['title']; ?>" class="col right-col">
-              <div class=" <?php echo $white; ?>">
-                  <img class="brandCultureImage" src="<?php echo $option['image']; ?>">
-                  <h4 class="brandCultureTitle"><?php echo $option['title']; ?></h4>
-                  <h5 class="brandCultureValues"><?php echo $option['keywords']; ?></h5>
-              </div>
-            </a>
-            <div id="<?php echo $option['title']; ?>" class="d-none">
-              <div class="lightboxBrandCultureBox">
-                  <h4 class="csh">
-                      <?php echo $option['title']; ?>
-                  </h4>
-                  <h5 class="brandCultureValues"><?php echo $option['keywords']; ?></h5>
-                  <p><?php echo $option['description']; ?></p>
-              </div>
+                <a class="col-xl-4 col-lg-6 col-md-6 col-sm-12 brandCultureContainer" data-fslightbox="custom-text"
+                    data-class="d-block" href="#<?php echo $option['title']; ?>" class="col right-col">
+                    <div class=" <?php echo $white; ?>">
+                        <img class="brandCultureImage" src="<?php echo $option['image']; ?>">
+                        <h4 class="brandCultureTitle"><?php echo $option['title']; ?></h4>
+                        <h5 class="brandCultureValues"><?php echo $option['keywords']; ?></h5>
+                    </div>
+                </a>
+                <div id="<?php echo $option['title']; ?>" class="d-none">
+                    <div class="lightboxBrandCultureBox">
+                        <h4 class="csh">
+                            <?php echo $option['title']; ?>
+                        </h4>
+                        <h5 class="brandCultureValues"><?php echo $option['keywords']; ?></h5>
+                        <p><?php echo $option['description']; ?></p>
+                    </div>
+                </div>
+                <?php endforeach; ?>
             </div>
-            <?php endforeach; ?>
         </div>
     </div>
-</div>
+</section>
 
-<div id="galerieAnchor" class="masonryContainer container-fluid sectionPadding">
-    <h2 class="text-center sectionPadding sectionTitle">GALERIE</h2>
-    <div class="gallery-items gallery-masonry">
-        <?php if ( have_rows( 'masonry_section' , 26897 ) ): ?>
+<section id="galerieAnchor">
+    <div class="masonryContainer container-fluid sectionPadding">
+        <h2 class="text-center sectionPadding sectionTitle">GALERIE</h2>
+        <div class="gallery-items gallery-masonry">
+            <?php if ( have_rows( 'masonry_section' , 26897 ) ): ?>
 
-        <?php while( have_rows( 'masonry_section', 26897 ) ) : the_row(); ?>
+            <?php while( have_rows( 'masonry_section', 26897 ) ) : the_row(); ?>
 
-        <?php if( $masonryImage = get_sub_field( 'masonry_image', 26897 ) ) { 
+            <?php if( $masonryImage = get_sub_field( 'masonry_image', 26897 ) ) { 
 
                 echo "	<div class='item'><a href='$masonryImage'><img src='$masonryImage'/></a></div>";
                 
                     } ?>
 
-        <?php endwhile; ?>
+            <?php endwhile; ?>
 
 
-        <?php endif; ?>
+            <?php endif; ?>
+        </div>
     </div>
-</div>
+</section>
 
-<div id="noutatiAnchor" class="container-fluid sectionPadding">
-    <h2 class="sectionTitle">NOUTĂȚI</h2>
-    <div class="newsContainer row">
-        <?php
+<section id="faq">
+    <div class="container-fluid faq p-relative sectionPadding">
+        <h2 class="sectionTitle">FAQ</h2>
+        <div class="container">
+            <div class="accordion" id="accordionExample">
+                <?php 
+            $questions = get_field("faq_repeater", "options"); 
+            $index = 1;
+            foreach($questions as $question): ?>
+                <div class="card">
+                    <div class="card-head" id="<?php echo "heading" . $index; ?>">
+                        <h2 class="mb-0 collapsed" data-toggle="collapse"
+                            data-target="<?php echo "#collapse" . $index; ?>" aria-expanded="false"
+                            aria-controls="<?php echo "collapse" . $index; ?>">
+                            <?php echo $question['question']; ?>
+                        </h2>
+                    </div>
+
+                    <div id="<?php echo "collapse" . $index; ?>" class="collapse"
+                        aria-labelledby="<?php echo "collapse" . $index; ?>" data-parent="#accordionExample">
+                        <div class="card-body">
+                            <?php echo $question['answer']; ?>
+                        </div>
+                    </div>
+                </div>
+                <?php $index++; ?>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="noutatiAnchor">
+    <div class="container-fluid sectionPadding">
+        <h2 class="sectionTitle">NOUTĂȚI</h2>
+        <div class="newsContainer row">
+            <?php
             $args = array('posts_per_page' => 3, 'orderby' => 'post_date', 'category_name' => 'noutati');
             $postslist = get_posts($args);
             foreach ($postslist as $post) : {
@@ -218,53 +256,9 @@
             endforeach;
             ?>
 
+        </div>
     </div>
-</div>
-
-<div class="container-fluid faq p-relative sectionPadding">
-<h2 class="sectionTitle">FAQ's</h2>
-<div class="container">
-  <div class="accordion" id="accordionExample">
-  <div class="card">
-    <div class="card-head" id="headingOne">
-      <h2 class="mb-0" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Question 1
-      </h2>
-    </div>
-
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-head" id="headingTwo">
-      <h2 class="mb-0 collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Question 2
-      </h2>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-head" id="headingThree">
-      <h2 class="mb-0 collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Question 3
-      </h2>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-</div>
+</section>
 
 <script src="/wp-content/themes/Divi-child/js/fslightbox.js"></script>
 <?php get_footer('codru2023live'); ?>
