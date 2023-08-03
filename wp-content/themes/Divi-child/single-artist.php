527af16ea4b551fab61f31c23206d6c761020c50
sebastiang07 <sebastian@verticalhq.ca>
2023-08-03 14:38:00 +0300

    Update artist page

--- wp-content/themes/Divi-child/single-artist.php
+++ wp-content/themes/Divi-child/single-artist.php
@@ -7,12 +7,14 @@ get_header('codru2023live');
 <div class="container-fluid singlePostContainer singleArtistContainer">
     <div class="container p-0">
         <div class="row sectionPadding">
-            <div class="col-lg-8 col-md-8 col-12">
+            <div class="col-lg-8 col-md-8 col-12 artistImageContainer">
                 <img class="singlePostMainImg" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
                     <h1><?php echo get_the_title(); ?></h1>
             </div>
             <div class="col-lg-4 col-md-4 col-12 artistSocialsSpotify">
-                <div class="spotifyIframeContainer" style="left: 0; width: 100%; min-height:30vh; position: relative;"><?php echo get_field('spotify_iframe')?></div>
+                <div class="spotifyIframeContainer">
+                    <?php echo get_field('spotify_iframe')?>
+                </div>
                     <span class="row pl-3 pr-3">
                     <?php if ( have_rows( 'social_repeater' ) ): ?>
             
@@ -22,7 +24,7 @@ get_header('codru2023live');
             
                                     $socialURL = get_sub_field( 'social_url' );
             
-                                    echo "<a class='col-lg-2 col-md-2 col-3 artistSocials' href='$socialURL'><img src='$socialImg' /></a>";
+                                    echo "<a class='col-lg-2 col-md-2 col-3 artistSocials' href='$socialURL' target='_blank'><img src='$socialImg' /></a>";
                                     
                                         } ?>
             
