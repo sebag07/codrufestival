6b2216ddb3908d95284de9a4d7d45e2794920b19
sebastiang07 <sebastian@verticalhq.ca>
2023-11-26 18:21:43 +0200

    Changed CSS version

--- wp-content/themes/Divi-child/header.php
+++ wp-content/themes/Divi-child/header.php
@@ -26,13 +26,13 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
     <script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
-    <link rel="stylesheet" href="/wp-content/themes/Divi-child/js/main.js?ver=157">
-    <script src="/wp-content/themes/Divi-child/js/mainCodru2023.js?ver=157"></script>
+    <link rel="stylesheet" href="/wp-content/themes/Divi-child/js/main.js?ver=158">
+    <script src="/wp-content/themes/Divi-child/js/mainCodru2023.js?ver=158"></script>
     <script src="/wp-content/themes/Divi-child/snow3d/build/snow3d.js"></script>
 
     <!-- Style -->
-    <link rel="stylesheet" href="/wp-content/themes/Divi-child/styleCodru2023live.css?ver=157">
-    <link rel="stylesheet" href="/wp-content/themes/Divi-child/assets/css/schedule-2023.css?ver=157">
+    <link rel="stylesheet" href="/wp-content/themes/Divi-child/styleCodru2023live.css?ver=158">
+    <link rel="stylesheet" href="/wp-content/themes/Divi-child/assets/css/schedule-2023.css?ver=158">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css"
         integrity="sha512-WEQNv9d3+sqyHjrqUZobDhFARZDko2wpWdfcpv44lsypsSuMO0kHGd3MQ8rrsBn/Qa39VojphdU6CMkpJUmDVw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
