024595d01387f8cffa9f30ca75db30659308a01c
sebastiang07 <sebastian@verticalhq.ca>
2024-06-05 12:43:27 +0300

    Added CTA on fii Frate cu Codru

--- wp-content/themes/Divi-child/templates/fii-frate-cu-codru-template.php
+++ wp-content/themes/Divi-child/templates/fii-frate-cu-codru-template.php
@@ -12,15 +12,15 @@
 
             <?php while( have_rows( 'value_repeater' ) ) : the_row(); ?>
 
-            <?php if( $repeaterImg = get_sub_field('value_number') ) { 
-
-                        echo "<div class='valueRepeater'>
-                            <p class='value-step'>$repeaterImg</p>";
-                            echo "<div class='value-text'>";
-                            the_sub_field('value_text');
-                            echo "</div>";
-                            echo "</div>";            
-                    } ?>
+            <?php
+                $repeaterCounter = get_sub_field('value_number');
+                    echo "<div class='valueRepeater'>
+                            <p class='value-step'>$repeaterCounter</p>";
+                    echo "<div class='value-text'>";
+                    the_sub_field('value_text');
+                    echo "</div>";
+                    echo "</div>";
+                ?>
 
             <?php endwhile; ?>
 
@@ -28,6 +28,9 @@
         </div>
         <div class="col-lg-6 col-md-4 col-12 partnerImage">
             <img src="/wp-content/themes/Divi-child/images/rightimg.png" alt="">
+        </div>
+        <div class="col-lg-12 col-md-12 col-12 text-center partnersTitle valueRepeater" style="justify-content: center">
+            <h2>Dacă și compania ta crede în aceste valori, contactează-ne acum!</h2>
         </div>
     </div>
 </div>
