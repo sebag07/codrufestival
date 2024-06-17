c56aa4b45da67cdad3735534768e0cbb10aa96f6
sebastiang07 <sebastian@verticalhq.ca>
2024-06-17 13:27:55 +0300

    Partners and single page CSS

--- wp-content/themes/Divi-child/templates/artists-page.php
+++ wp-content/themes/Divi-child/templates/artists-page.php
@@ -1,31 +1,67 @@
 <?php /*  Template Name: Artisti  */ ?>
 <?php get_header(); ?>
 
-<div class="container apeluriArtisti termsPage pt-5 pb-5">
+<div class="container-fluid single-page header-padding">
 
-    <h1 class="pt-5 pb-4 text-center" style="font-weight: 600;"><?php echo get_the_title(); ?></h1>
-    <div class="newsContainer row">
-        <?php
-        $args = array(
-            'orderby'           => 'post_date',
-            'order'             => 'DESC',
-            'category_name'     => 'apeluri-artisti',
-            'exclude'           => array(get_the_id()));
-        $postslist = get_posts($args);
-        foreach ($postslist as $post) : {
-            $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
-            $read_more = get_field('news_read_more', 'options');
-            echo "<div class='homepageNews col-lg-4 col-md-6 col-12'>
-            <a href='$post->guid' class='homepageNewsLink'>
-            <div class='homepageNewsImage text-center'><img src='$image[0]' alt=''></div>
-            <div class='homepageNewsTitle'><h3>$post->post_title</h3><span><img src='/wp-content/themes/Divi-child/images/right-chevron.png' />$read_more</span></div>
-            </a>
-        </div>";
-        }
-        endforeach;
-        ?>
+    <h1 class="text-center" style="font-weight: 600;"><?php echo get_the_title(); ?></h1>
+    <section>
+        <div class="sectionPadding container">
+            <div class="artistCardContainer">
+                <?php
+                $args = array('posts_per_page' => -1, 'orderby' => 'title', 'suppress_filters' => false, 'order' => 'ASC', 'post_type' => 'artist');
+                $postslist = get_posts($args);
+                foreach ($postslist as $post) :
+                    $artistName = get_the_title();
+                    $artistPage = get_the_permalink();
+                    $artistImage = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail');
+                    if ($artistImage) {
+                        $imageUrl = $artistImage[0];
+                    } else {
+                        $imageUrl = "/wp-content/themes/Divi-child/images/logo.png";
+                    }
+                    ?>
+                    <a href='<?php echo $artistPage; ?>' class='artistInnerContainer'>
+                        <div class='artistImageContainer'>
+                            <img class='artistImg' loading='lazy' src='<?php echo $imageUrl; ?>'>
+                            <div class='imageOverlay'></div>
+                        </div>
+                        <div class='artistContent'>
+                            <div class='artistContentBG'></div>
+                            <div class='artistContentMeta'>
+                            <span class='artistContentName'>
+                                <?php echo $artistName; ?>
+                            </span>
+                                <span class='artistContentDayStage'>
+                                    test
+                                </span>
+                            </div>
+                        </div>
+                        <div class='artistCardHoverOverlay'></div>
 
-    </div>
+                        <div class='artistCardReadMoreBtn'><?php echo get_field('artists_card_button', 'options'); ?></div>
+
+                    </a>
+                <?php endforeach; ?>
+            </div>
+        </div>
+        <script>
+
+            jQuery(".artistInnerContainer").hover(
+                function () {
+                    jQuery(this).find('.artistCardReadMoreBtn').addClass('readMoreBtnHover');
+                    jQuery(this).find('.artistContent').addClass('artistContentTop');
+                    jQuery(this).find('.artistCardHoverOverlay').addClass('onHoverOverlayOpacity');
+                    jQuery(this).addClass('borderOnHover');
+                },
+                function () {
+                    jQuery(this).find('.artistCardReadMoreBtn').removeClass('readMoreBtnHover');
+                    jQuery(this).find('.artistContent').removeClass('artistContentTop');
+                    jQuery(this).find('.artistCardHoverOverlay').removeClass('onHoverOverlayOpacity');
+                    jQuery(this).removeClass('borderOnHover');
+                }
+            );
+        </script>
+    </section>
 </div>
 
 
