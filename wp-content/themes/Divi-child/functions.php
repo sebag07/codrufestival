930c44a77bd0cd14b43f3d815c49c3b5f96dc73c
sebastiang07 <sebastian@verticalhq.ca>
2024-07-10 20:10:42 +0300

    Added taxonomy that excludes the other languages

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -4,15 +4,18 @@ require get_template_directory() . '-child/includes/widgets/activities.php';
 require get_template_directory() . '-child/includes/widgets/schedule.php';
 require get_template_directory() . '-child/includes/widgets/partners.php';
 
-function my_theme_enqueue_styles() { 
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), '150');
-    wp_enqueue_style( 'activities', get_template_directory_uri() . '-child' . '/assets/css/activities.css');
-    wp_enqueue_style( 'partners', get_template_directory_uri() . '-child' . '/assets/css/partners.css');
-    wp_enqueue_style( 'magnificPopupCss', get_template_directory_uri() . '-child' . '/assets/css/magnific-popup.min.css');
+function my_theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css', array(), '150');
+    wp_enqueue_style('activities', get_template_directory_uri() . '-child' . '/assets/css/activities.css');
+    wp_enqueue_style('partners', get_template_directory_uri() . '-child' . '/assets/css/partners.css');
+    wp_enqueue_style('magnificPopupCss', get_template_directory_uri() . '-child' . '/assets/css/magnific-popup.min.css');
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
-function load_child_scripts(){
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+function load_child_scripts()
+{
     wp_register_script('main', get_template_directory_uri() . "-child" . '/js/main.js', array('jquery'), '4123312', true);
     wp_enqueue_script('main');
     wp_register_script('play-song', get_template_directory_uri() . "-child" . '/js/playsong.js', array('jquery'), true);
@@ -27,9 +30,10 @@ function load_child_scripts(){
     wp_enqueue_script('magnificPopup');
 }
 
-add_action('wp_enqueue_scripts', 'load_child_scripts'); 
+add_action('wp_enqueue_scripts', 'load_child_scripts');
 
-function load_footer_scripts(){
+function load_footer_scripts()
+{
     wp_register_script('util', get_template_directory_uri() . "-child" . '/js/util.js', array('jquery'), true);
     wp_enqueue_script('util');
     wp_register_script('program', get_template_directory_uri() . "-child" . '/js/program.js', array('jquery'), true);
@@ -39,14 +43,14 @@ function load_footer_scripts(){
 add_action('wp_footer', 'load_footer_scripts');
 
 
-
-function get_menu_with_children($menu_name){
+function get_menu_with_children($menu_name)
+{
     $navbar_items = wp_get_nav_menu_items($menu_name);
     $menu_with_children = array();
     $child_items = [];
 
     // pull all child menu items into separate object
-    foreach ( (array) $navbar_items as $key => $item) {
+    foreach ((array)$navbar_items as $key => $item) {
         if ($item->menu_item_parent) {
             array_push($child_items, $item);
             unset($navbar_items[$key]);
@@ -54,7 +58,7 @@ function get_menu_with_children($menu_name){
     }
 
     // push child items into their parent item in the original object
-    foreach ( (array) $navbar_items as $item) {
+    foreach ((array)$navbar_items as $item) {
         foreach ($child_items as $key => $child) {
             if ($child->menu_item_parent == $item->ID) {
                 if (!$item->child_items) {
@@ -70,70 +74,88 @@ function get_menu_with_children($menu_name){
     return $navbar_items;
 }
 
-if( function_exists('acf_add_options_page') ) {
-    
+if (function_exists('acf_add_options_page')) {
+
     acf_add_options_page(array(
-        'page_title'    => 'Options',
-        'menu_title'    => 'Options',
-        'menu_slug'     => 'general-options',
-        'capability'    => 'edit_posts',
-        'redirect'      => false
-    ));
-    
-    acf_add_options_sub_page(array(
-        'page_title'    => 'Partners',
-        'menu_title'    => 'Partners',
-        'menu_slug'     => 'partners-options',
-        'capability'    => 'edit_posts',
-        'redirect'      => false,
-        'parent_slug'   => 'general-options',
+        'page_title' => 'Options',
+        'menu_title' => 'Options',
+        'menu_slug' => 'general-options',
+        'capability' => 'edit_posts',
+        'redirect' => false
     ));
 
     acf_add_options_sub_page(array(
-        'page_title'    => 'Newsletter',
-        'menu_title'    => 'Newsletter',
-        'menu_slug'     => 'newsletter-options',
-        'capability'    => 'edit_posts',
-        'redirect'      => false,
-        'parent_slug'   => 'general-options',
+        'page_title' => 'Partners',
+        'menu_title' => 'Partners',
+        'menu_slug' => 'partners-options',
+        'capability' => 'edit_posts',
+        'redirect' => false,
+        'parent_slug' => 'general-options',
     ));
 
     acf_add_options_sub_page(array(
-        'page_title'    => 'Stages',
-        'menu_title'    => 'Stages',
-        'menu_slug'     => 'stages-options',
-        'capability'    => 'edit_posts',
-        'redirect'      => false,
-        'parent_slug'   => 'general-options',
+        'page_title' => 'Newsletter',
+        'menu_title' => 'Newsletter',
+        'menu_slug' => 'newsletter-options',
+        'capability' => 'edit_posts',
+        'redirect' => false,
+        'parent_slug' => 'general-options',
     ));
-    
-  }
 
-  add_filter( 'the_content', 'wpautop' );
+    acf_add_options_sub_page(array(
+        'page_title' => 'Stages',
+        'menu_title' => 'Stages',
+        'menu_slug' => 'stages-options',
+        'capability' => 'edit_posts',
+        'redirect' => false,
+        'parent_slug' => 'general-options',
+    ));
 
-function add_custom_rewrite_rule() {
+}
+
+add_filter('the_content', 'wpautop');
+
+function add_custom_rewrite_rule()
+{
     add_rewrite_rule(
         '^informatii/([^/]*)/?',
         'index.php?pagename=informatii&filter=$matches[1]',
         'top'
     );
 }
+
 add_action('init', 'add_custom_rewrite_rule');
 
-function register_query_vars( $vars ) {
+function register_query_vars($vars)
+{
     $vars[] = 'filter';
     return $vars;
 }
+
 add_filter('query_vars', 'register_query_vars');
 
 
-function display_artists_by_level($category_name, $exclude_post_id = null) {
-    $args = array('posts_per_page' => -1, 'orderby' => 'title', 'order' => 'ASC', 'post_type' => 'artist', 'category_name' => $category_name,'post_status' => 'publish', 'exclude' => $exclude_post_id );
+function display_artists_by_level($category_name, $exclude_post_id = null)
+{
+    $args = array(
+        'posts_per_page' => -1,
+        'orderby' => 'title',
+        'order' => 'ASC',
+        'post_type' => 'artist',
+        'category_name' => $category_name,
+        'post_status' => 'publish',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'wpml_language',
+                'field' => 'slug',
+                'terms' => ICL_LANGUAGE_CODE, // Filter by the current language
+            ),
+        ),);
     $postslist = get_posts($args);
     foreach ($postslist as $key => $post) {
         setup_postdata($post); // Set up post data for use in the loop (important)
         $artistName = get_the_title($post->ID);
-        if($key === array_key_last($postslist)){
+        if ($key === array_key_last($postslist)) {
             echo "<div class='artists-name'><h4 class='m-0 pb-0' style='color: var(--artist-level-color-secondary);'>$artistName </h4></div>";
         } else {
             echo "<div class='artists-name'><h4 class='m-0 pb-0' style='color: var(--artist-level-color-secondary);'>$artistName</h4></div><div class='artists-bullet'><span style='margin-left: 5px; margin-right: 5px;'>&bull;</span></div>";
