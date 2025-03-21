186a9c0341b467b7fec84f9ec60affc2b0f7d88b
sebastiang07 <sebastian@verticalhq.ca>
2025-03-21 12:18:32 +0200

    Removed UIPress

--- wp-content/plugins/uipress-lite/app/dist/uipfrontend.build.D-IQp8E9.js
+++
@@ -1,2 +0,0 @@
-import{s as e}from"./assets/setupApp-erJstP6L.js";import"./assets/index-COn-5JHg.js";const o=document.querySelector("#wpadminbar");if(o){const t=document.createElement("div");t.id="uip-frontend-toolbar",o.appendChild(t),e(t)}
-//# sourceMappingURL=uipfrontend.build.D-IQp8E9.js.map
