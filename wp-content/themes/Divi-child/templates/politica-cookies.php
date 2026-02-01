fb6f626edf0f74d61a468434f16c17f26813273a
sebastiang07 <sebastian@verticalhq.ca>
2026-02-01 15:01:11 +0200

    Added cookie declaration

--- wp-content/themes/Divi-child/templates/politica-cookies.php
+++ wp-content/themes/Divi-child/templates/politica-cookies.php
@@ -4,6 +4,7 @@
 <div class="container termsPage pt-5 pb-5">
     <h1 class="pt-5 pb-4 text-center"><?php echo get_the_title(); ?></h1>
     <div>
+        <script id="CookieDeclaration" src="https://consent.cookiebot.com/45b7bb1e-0efb-4c36-bac6-351f23f29ceb/cd.js" type="text/javascript" async></script>
     </div>
 </div>
 
