a2440559352be63324cdd7075cdb53ba40410a7a
sebastiang07 <sebastian@verticalhq.ca>
2026-02-01 14:59:53 +0200

    Updated cookiebot code

--- wp-content/themes/Divi-child/header.php
+++ wp-content/themes/Divi-child/header.php
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Winky+Sans:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="45b7bb1e-0efb-4c36-bac6-351f23f29ceb" data-blockingmode="auto" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS -->
     <link rel="icon" type="image/png" href="/wp-content/themes/Divi-child/images/favicon-codru.png">
