463a47ac3fe96b079cb1219a722d37bb519cf611
sebastiang07 <sebastian@verticalhq.ca>
2026-07-07 16:53:06 +0300

    Changed title 28-30

--- wp-content/themes/codrufestival/header.php
+++ wp-content/themes/codrufestival/header.php
@@ -51,7 +51,7 @@
         }
     </style>
 
-    <title>CODRU Festival | 28 - 30 august 2026, Pădurea Verde, Timișoara, Romania</title>
+    <title>CODRU Festival | 28-30 august 2026, Pădurea Verde, Timișoara, Romania</title>
 
     <!-- Meta Pixel Code -->
     <script>
