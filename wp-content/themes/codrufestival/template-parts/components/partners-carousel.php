f2e3b4a28c45df1e41c9fcdb95442e2bc01aca24
sebastiang07 <sebastian@verticalhq.ca>
2026-08-05 11:05:39 +0300

    Added bag partnersLevel5

--- wp-content/themes/codrufestival/template-parts/components/partners-carousel.php
+++ wp-content/themes/codrufestival/template-parts/components/partners-carousel.php
@@ -21,12 +21,11 @@ $partner_groups = [
         'name_field' => 'partner_name',
         'image_field' => 'partner_image',
     ],
-    // TEMPORARY: partners_level_5 hidden — uncomment to show again
-    // [
-    //     'acf_key' => 'partners_level_5',
-    //     'name_field' => 'partner_name',
-    //     'image_field' => 'partner_image',
-    // ],
+    [
+        'acf_key' => 'partners_level_5',
+        'name_field' => 'partner_name',
+        'image_field' => 'partner_image',
+    ],
     [
         'acf_key' => 'produced_by',
         'name_field' => 'producer_name',
