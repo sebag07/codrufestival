b8314b184f89959262dd5a58277ddc40ad82d0bc
sebastiang07 <sebastian@verticalhq.ca>
2026-08-25 21:32:44 +0300

    Changed calendar to new one

--- wp-content/themes/codrufestival/templates/program.php
+++ wp-content/themes/codrufestival/templates/program.php
@@ -4,7 +4,7 @@
 <div class="container programPage termsPage pt-5 pb-5">
     <h1 class="pt-5 pb-4 text-center"><?php echo get_the_title(); ?></h1>
     <div>
-        <?php echo do_shortcode('[festivawl_calendar id="116"]'); ?>
+        <?php echo do_shortcode('[festivawl_calendar id="569"]'); ?>
     </div>
 </div>
 
